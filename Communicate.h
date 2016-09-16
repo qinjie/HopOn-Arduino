b25f94654278045efe39037128ae19bf45079603
Intern <intern@QJ-Mac.local>
2016-09-16 20:49:11 +0800

    Refactor code

---
+++ Communicate.h
@@ -0,0 +1,23 @@
+const int DEBUG = 0;
+const int RESET_MEMORY = 0;
+int speakerPin = 1;
+int buttonPin = A1;
+String llb_serial = "SG11116";
+
+typedef enum BicycleState {
+  free_state = 0,
+  locked_state = 1,
+  unlocked_state = 2
+};
+
+typedef enum Action {
+  unlock_bicycle = 1,
+  lock_bicycle = 2,
+  return_bicycle = 3,
+  get_state = 4
+};
+
+template <typename T>
+inline void logMsg(T msg) {
+  if (DEBUG) Serial.println(msg);
+}
--- Communicate.ino
+++ Communicate.ino
@@ -1,16 +1,19 @@
 #include <MD5.h>
 #include<EEPROM.h>
 #include "pitch.h"
+#include "Communicate.h"
 
-int state = 0;
-int speakerPin = 5;
-int melody[] = {NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+int buttonState = 0;  // HIGH: off, LOW: on
+
+int melodyUnlock[] = {NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+int melodyLock[] = {NOTE_C4, NOTE_B3, 0, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_G3, NOTE_C4};
+int melodyReturn[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_A3, NOTE_G3, NOTE_G3, NOTE_C4}; 
 int noteDurations[] = {4, 8, 8, 4,4,4,4,4 };
-String llb_serial = "SG11111";
+
+BicycleState state = free_state;
 String hash_in, user_in, action_in;
+Action action;
 String savedHash, savedUserId;
-// State = { 0: free, 1: locked, 2: unlocked }
-// Action = { 0: unlock, 1: lock, 2: return }
 
 String readHashEEPROM() {
   String res = "";
@@ -21,7 +24,6 @@ String readHashEEPROM() {
   }
   return res;
 }
-
 void writeHashEEPROM(String hash) {
   for (int addr = 0; addr < 32; ++addr) {
     EEPROM.write(addr, hash.charAt(addr));
@@ -37,19 +39,17 @@ String readUserIdEEPROM() {
   }
   return res;
 }
-
 void writeUserIdEEPROM(String userId) {
   for (int i = 0; i < userId.length(); ++i) {
     EEPROM.write(64 + i, userId.charAt(i));
   }
 }
 
-int readBikeStateEEPROM() {
-  return EEPROM.read(99);
+BicycleState readBicycleStateEEPROM() {
+  return static_cast<BicycleState>(EEPROM.read(99));
 }
-
-void writeBikeStateEEPROM(int bikeState) {
-  EEPROM.write(99, bikeState);
+void writeBicycleStateEEPROM(BicycleState state) {
+  EEPROM.write(99, state);
 }
 
 void resetEEPROM() {
@@ -60,6 +60,9 @@ void resetEEPROM() {
 
 void setup() {
   Serial.begin(9600);
+  pinMode(speakerPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, HIGH);
   
   Bean.enableMotionEvent(ORIENT_EVENT);
   uint8_t mode = Bean.getAccelerometerPowerMode();
@@ -67,137 +70,146 @@ void setup() {
     Bean.accelRegisterWrite(REG_POWER_MODE_X11, VALUE_LOW_POWER_10MS);
   }
 
-//  resetEEPROM();
-  // Get hash and user id from EEPROM
+  if (RESET_MEMORY) resetEEPROM();
+  
   savedHash = readHashEEPROM();
   savedUserId = readUserIdEEPROM();
-  state = readBikeStateEEPROM();
-  Serial.println(state);
-  Serial.println("savedHash ." + savedHash + ".");
-  Bean.setScratchNumber(1, state);
+  state = readBicycleStateEEPROM();
+  respondState();
+  logMsg("savedHash ." + savedHash + "."); 
+  logMsg("Started");
   
-  Serial.println("Started");
   Bean.setLed(0, 0, 0);
+  tone(speakerPin, 2000, 200);
+  delay(300);
+  noTone(speakerPin);
+}
+
+void respondState() {
+  switch (state) {
+    case free_state: Serial.print("0"); break;
+    case locked_state: Serial.print("1"); break;
+    case unlocked_state: Serial.print("2"); break;
+  }
+}
+
+void unlockBicycle() {
+  state = unlocked_state;
+  
+  soundBuzzer(unlock_bicycle);
+  // Blink Green
+  Bean.setLed(0, 255, 0);
+  Bean.sleep(1000);
+  Bean.setLed(0, 0, 0);
+
+  writeBicycleStateEEPROM(state);
+  respondState();
+  logMsg("Unlocked!");
+}
+
+void lockBicycle() {
+  state = locked_state;
+  
+  soundBuzzer(lock_bicycle);
+  // Blink Red
+  Bean.setLed(255, 0, 0);
+  Bean.sleep(1000);
+  Bean.setLed(0, 0, 0);
+
+  writeBicycleStateEEPROM(state);
+  respondState();
+  logMsg("Locked!");
+}
+
+void returnBicycle() {
+  state = free_state;
+  
+  soundBuzzer(return_bicycle);
+  // Blink Blue
+  Bean.setLed(0, 0, 255);
+  Bean.sleep(1000);
+  Bean.setLed(0, 0, 0);
+  
+  resetEEPROM();
+  respondState();
+  logMsg("Returned!");
 }
 
 void loop() {
-  Serial.println("loop");
+  logMsg("loop"); 
   if (validProtocol()) {
+    logMsg("valid protocol");
     // Save hash and user id
     if (savedHash.length() == 0) {
       writeHashEEPROM(hash_in);
       writeUserIdEEPROM(user_in);
     }
     
-    // Unlock when bicycle is free or locked
-    if (action_in == "0" && (state == 0 || state == 1)){
-      soundBuzzer();
-
-      // Bicycle is unlocked
-      state = 2;
-      Serial.println("Unlocked!");
-
-      // Blink Green
-      Bean.setLed(0, 255, 0);
-      Bean.sleep(2000);   
-      Bean.setLed(0, 0, 0);
-
-      writeBikeStateEEPROM(state);
-      Bean.setScratchNumber(1, state);
-    }
-
-    // Lock when bicycle is unlocked
-    if (action_in == "1" && state == 2){
-      soundBuzzer();
-
-      // Bicycle is locked
-      state = 1;
-      Serial.println("Locked!");
-
-      // Blink Red
-      Bean.setLed(255, 0, 0);
-      Bean.sleep(2000);  
-      Bean.setLed(0, 0, 0);
-
-      writeBikeStateEEPROM(state);
-      Bean.setScratchNumber(1, state);
-    }
-
-    // Return when bicycle is locked or unlocked
-    if (action_in == "2" && (state == 1 || state == 2)){
-      soundBuzzer();
-
-      // Bicycle is free
-      state = 0;
-      Serial.println("Returned!");
-
-      // Blink Blue
-      Bean.setLed(0, 0, 255);
-      Bean.sleep(2000);  
-      Bean.setLed(0, 0, 0);
-      
-      resetEEPROM();
-      Bean.setScratchNumber(1, state);
-    }
+    if (action == unlock_bicycle 
+      && (state == free_state || state == locked_state)) unlockBicycle();
+    else if (action == lock_bicycle && state == unlocked_state) lockBicycle();
+    else if (action == return_bicycle 
+      && (state == unlocked_state || state == locked_state)) returnBicycle();
+    else if (action == get_state) respondState();
   }
 
-  // When bicycle is free or locked
-  if (state == 0 || state == 1) {
+  buttonState = digitalRead(buttonPin);
+  logMsg(buttonState);
+  if (buttonState == LOW && state == unlocked_state) {
+    lockBicycle();
+  }
+  
+  if (state == free_state || state == locked_state) {
     bool isMoving = Bean.checkMotionEvent(ORIENT_EVENT);
-    while (isMoving) {
+    if (isMoving) {
+      Serial.print("-1");
       alertBuzzer();
-      isMoving = Bean.checkMotionEvent(ORIENT_EVENT);
     }
   }
-  Bean.sleep(0xFFFFFFFF);
+  Bean.sleep(2000);
 }
 
 bool validProtocol() {
-  String data = Serial.readString();
-  char arraydata[46];
-  char data_array[3][46];
-  data.toCharArray(arraydata, 46);
-  int count = 0;
-  int index = 0;
-  for (int i=0; i< 46; i++){
-    if (data[i] != '\0'){
-      if (data[i] == ','){
-        data_array[index][count] = '\0';
-        count = 0;
-        index++;
-      }
-      else{
-        data_array[index][count] = data[i];
-        count++;
-      }
-    }
-    else{
-      break;
-    }
-  }
-  data_array[index][count] = '\0';
-  hash_in = String(data_array[0]);
-  user_in = String(data_array[1]);
-  action_in = String(data_array[2]);
+  String data = Serial.readString();  
+  hash_in = "";
+  user_in = "";
+  action_in = "";
+  int firstComma = data.indexOf(',');
+  if (firstComma == -1) return false;
+  hash_in = data.substring(0, firstComma);
+  int secondComma = data.indexOf(',', firstComma + 1);
+  if (secondComma == -1) return false;
+  user_in = data.substring(firstComma + 1, secondComma);
+  action_in = data.substring(secondComma + 1);
+  
   if (savedHash.length() == 32) {
     return (hash_in == savedHash && user_in == savedUserId);
   } else {
     String hash_out = llb_serial + "," + user_in;
-    int len = user_in.length() + llb_serial.length() + 2;
-    char arrayhash[len];
-    hash_out.toCharArray(arrayhash, len);
-    unsigned char* hash=MD5::make_hash(arrayhash);
+    unsigned char* hash=MD5::make_hash((char*)hash_out.c_str());
     char *md5str = MD5::make_digest(hash, 16);
+    logMsg("check " + String(md5str));
+    bool result = String(md5str) == hash_in;
     free(hash);
-    Serial.println("check " + String(md5str));
-    return String(md5str) == hash_in; 
+    free(md5str);
+    int temp = action_in.toInt();
+    if (temp == 0) result = false;
+    else action = static_cast<Action>(temp);
+    return result;
   }
 }
 
-void soundBuzzer() {
+void soundBuzzer(Action action) {
   for (int thisNote = 0; thisNote < 8; thisNote++) {
     int noteDuration = 1000/noteDurations[thisNote];
-    tone(speakerPin, melody[thisNote],noteDuration);
+    switch (action) {
+      case unlock_bicycle:
+        tone(speakerPin, melodyUnlock[thisNote], noteDuration); break;
+      case lock_bicycle:
+        tone(speakerPin, melodyLock[thisNote], noteDuration); break;
+      case return_bicycle:
+        tone(speakerPin, melodyReturn[thisNote], noteDuration); break;
+    }
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     noTone(speakerPin);
@@ -205,16 +217,14 @@ void soundBuzzer() {
 }
 
 void alertBuzzer() {
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-    int noteDuration = 1000/noteDurations[thisNote];
+  for (int thisNote = 0; thisNote < 4; thisNote++) {
     if (thisNote & 1) { // is odd note
       Bean.setLed(0, 0, 255);
     } else {  // is even note
       Bean.setLed(255, 0, 0);
     }
-    tone(speakerPin, melody[thisNote],noteDuration);
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
+    tone(speakerPin, 2000, 300);
+    delay(400);
     noTone(speakerPin);
   }
   Bean.setLed(0, 0, 0);
